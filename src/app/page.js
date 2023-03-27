d56db8fcc56b56adebbcefed06efc5fcef16d5f9
briyk <mhmdbriyk@gmail.com>
2023-03-28 00:11:18 +0200

    add tailwind css

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,11 @@
 import Image from 'next/image'
-import { Roboto } from 'next/font/google'
 
-const roboto = Roboto({
-  weight: '400',
-  subsets: ['latin'],
-})
+
 
 export default function Home() {
   return (
     <main>
-      <h1>Home</h1>
+      <h1 className="text-gray-300 text-3xl">Home</h1>
     </main>
   )
 }
